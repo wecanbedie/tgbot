496dd1bc8d63cfaf6c1ddd969a8ceb64176efc13
wecanbedie <77538030+wecanbedie@users.noreply.github.com>
2023-07-07 12:21:26 +0300

    Исправления fetch_nasa_picture.py
    - Дал пользователю возможность изменять данные

--- fetch_nasa_pictures.py
+++ fetch_nasa_pictures.py
@@ -1,15 +1,16 @@
 import requests
 import os.path
+import argparse
 
 from dotenv import load_dotenv
 from download_photo import download_photo
 from get_extension_link import get_file_format_link
 from pathlib import Path
 
-def fetch_nasa_pictures(nasa_api_key):
+def fetch_nasa_pictures(nasa_api_key, count):
     nasa_url = "https://api.nasa.gov/planetary/apod" 
     params = {"api_key": nasa_api_key, 
-    "count": 35}
+    "count": count}
     nasa_response = requests.get(nasa_url, params=params)
     nasa_pictures_links = nasa_response.json()
     for nasa_picture in nasa_pictures_links:
@@ -27,9 +28,13 @@ def fetch_nasa_pictures(nasa_api_key):
 def main():
     Path("images").mkdir(parents=True, exist_ok=True)
 
+    parser = argparse.ArgumentParser(description='Укажите количество фотографий.')
+    parser.add_argument('--count', type=int, help='Количество скачиваемых фотографий.', default=35)
+    args = parser.parse_args() 
+
     load_dotenv()
     nasa_api_key = os.environ["NASA_API_KEY"]
-    fetch_nasa_pictures(nasa_api_key)
+    fetch_nasa_pictures(nasa_api_key, args.count)
 
 
 if __name__ == "__main__":
