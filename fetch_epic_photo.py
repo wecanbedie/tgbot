27ae03f787fa569b14fe60250500e862e85acecd
wecanbedie <77538030+wecanbedie@users.noreply.github.com>
2023-07-07 12:22:59 +0300

    Изменения fetch_epic_photo.py
    - Дал пользователю возможность менять папку с файлами

--- fetch_epic_photo.py
+++ fetch_epic_photo.py
@@ -1,6 +1,7 @@
 import requests
 import os.path
 #import datetime
+import argparse
 
 from pathlib import Path
 from download_photo import download_photo
@@ -8,7 +9,7 @@ from get_extension_link import get_file_format_link
 from dotenv import load_dotenv
 from datetime import datetime
 
-def fetch_epic_photo(nasa_api_key):
+def fetch_epic_photo(nasa_api_key, folder):
     url = 'https://api.nasa.gov/EPIC/api/natural/images'
     params = {"api_key": nasa_api_key}
     response = requests.get(url, params=params)
@@ -19,7 +20,6 @@ def fetch_epic_photo(nasa_api_key):
         formated_image_date = datetime.fromisoformat(image_date).strftime("%Y/%m/%d")
         link = f'https://api.nasa.gov/EPIC/archive/natural/{formated_image_date}/png/{image_name}.png'
         filename, expansion = get_file_format_link(link)
-        folder = 'images'
         fullname = f'{filename}{expansion}'
         file_path = os.path.join(folder, fullname)
         download_photo(link, file_path, params)
@@ -27,12 +27,15 @@ def fetch_epic_photo(nasa_api_key):
  
 
 def main():
-    Path("images").mkdir(parents=True, exist_ok=True)
+    parser = argparse.ArgumentParser(description='Укажите ваш путь к папке с фотографиями.')
+    parser.add_argument('--folder', type=str, help='Путь к папке с фотографиями.', default='images')
+    args = parser.parse_args()
 
-    
+    Path(args.folder).mkdir(parents=True, exist_ok=True)
+
     load_dotenv()
     nasa_api_key = os.environ["NASA_API_KEY"]
-    fetch_epic_photo(nasa_api_key)
+    fetch_epic_photo(nasa_api_key, args.folder)
 
 
 if __name__=="__main__":
