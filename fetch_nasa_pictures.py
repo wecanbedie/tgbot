af3d212ed1f0abf3d0c51248334ec194f013e135
wecanbedie <77538030+wecanbedie@users.noreply.github.com>
2023-07-12 11:40:30 +0300

    Исправления fetch_nasa_picture.py
    - Дал пользователю возможность самостоятельно указывать путь к файлу

--- fetch_nasa_pictures.py
+++ fetch_nasa_pictures.py
@@ -7,7 +7,7 @@ from download_photo import download_photo
 from get_extension_link import get_file_format_link
 from pathlib import Path
 
-def fetch_nasa_pictures(nasa_api_key, count):
+def fetch_nasa_pictures(nasa_api_key, count, folder):
     nasa_url = "https://api.nasa.gov/planetary/apod" 
     params = {"api_key": nasa_api_key, 
     "count": count}
@@ -18,23 +18,22 @@ def fetch_nasa_pictures(nasa_api_key, count):
             nasa_picture_located_link = nasa_picture['url']
             filename, file_format = get_file_format_link(nasa_picture_located_link) 
             if not file_format:
-                continue
-            folder = 'images'    
+                continue    
             fullname = f'{filename}{file_format}'   
             file_path = os.path.join(folder, fullname)
             download_photo(nasa_picture_located_link, file_path)
 
 
 def main():
-    Path("images").mkdir(parents=True, exist_ok=True)
-
     parser = argparse.ArgumentParser(description='Укажите количество фотографий.')
     parser.add_argument('--count', type=int, help='Количество скачиваемых фотографий.', default=35)
+    parser.add_argument('--folder', type=str, help='Укажите путь к папке с фотографиями.', default='images')
     args = parser.parse_args() 
+    Path(args.folder).mkdir(parents=True, exist_ok=True)
 
     load_dotenv()
     nasa_api_key = os.environ["NASA_API_KEY"]
-    fetch_nasa_pictures(nasa_api_key, args.count)
+    fetch_nasa_pictures(nasa_api_key, args.count, args.folder)
 
 
 if __name__ == "__main__":
